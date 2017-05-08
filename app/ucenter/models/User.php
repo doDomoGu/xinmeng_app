fe05d261ca4ad34c10de72b56c69445e3494e6c2
doDomoGu <kaka_71936410@qq.com>
2017-05-08 21:57:03 +0800

    Sample 目录移动

--- app/ucenter/models/User.php
+++ app/ucenter/models/User.php
@@ -1,5 +1,5 @@
 <?php
-namespace common\models;
+namespace ucenter\models;
 
 use Yii;
 use yii\base\NotSupportedException;
@@ -7,29 +7,12 @@ use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 use yii\web\IdentityInterface;
 
-/**
- * User model
- *
- * @property integer $id
- * @property string $username
- * @property string $password_hash
- * @property string $password_reset_token
- * @property string $email
- * @property string $auth_key
- * @property integer $status
- * @property integer $created_at
- * @property integer $updated_at
- * @property string $password write-only password
- */
 class User extends ActiveRecord implements IdentityInterface
 {
-    const STATUS_DELETED = 0;
-    const STATUS_ACTIVE = 10;
+    const STATUS_DELETED = 2;
+    const STATUS_ACTIVE = 1;
+    const STATUS_BANNED = 0;
 
-
-    /**
-     * @inheritdoc
-     */
     public static function tableName()
     {
         return '{{%user}}';
