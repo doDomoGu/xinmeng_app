f0b8933d33be2e3fe1f6a0c0ed757dbd58e416fe
doDomoGu <kaka_71936410@qq.com>
2017-06-09 16:04:18 +0800

    数据表 迁移

--- console/migrations/m170609_060926_ucenter_app.php
+++ console/migrations/m170609_060926_ucenter_app.php
@@ -2,10 +2,8 @@
 
 use yii\db\Migration;
 
-class m002_ucenter_app_init extends Migration
+class m170609_060926_ucenter_app extends Migration
 {
-    public $message;
-
     public function init(){
         $this->db = Yii::$app->db_ucenter;
         parent::init();
@@ -13,20 +11,18 @@ class m002_ucenter_app_init extends Migration
 
     public function up()
     {
-        $this->message = 1;
-
         $this->createTable('{{%app}}', [
-            'id' => $this->integer(11)->notNull(),
-            'name' => $this->integer(11)->notNull(),
-            'alias' => $this->integer(11)->notNull(),
-            'is_enable' => $this->smallInteger(1)->notNull()
+            'id' => $this->primaryKey(11),
+            'name' => $this->string(200)->notNull(),
+            'alias' => $this->string(200)->notNull(),
+            'status' => $this->smallInteger(1)->notNull()
         ]);
 
 
         $this->createTable('{{%user_app_auth}}', [
             'app_id' => $this->integer(11)->notNull(),
             'user_id' => $this->integer(11)->notNull(),
-            'is_enable' => $this->smallInteger(1)->notNull()
+            'status' => $this->smallInteger(1)->notNull()
         ]);
         $this->addPrimaryKey('primary_key','{{%user_app_auth}}',['app_id','user_id']);
     }
